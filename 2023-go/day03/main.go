270c4f71a08a14fd1dcd132b61df6bf7e3f7bab1
Ilea Cristian <ileacristian@gmail.com>
2024-01-23 19:49:10 +0200

    WIP day3

--- 2023-go/day03/main.go
+++ 2023-go/day03/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	file, err := os.Open("day03.txt")
+	file, err := os.Open("test.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -33,10 +33,11 @@ func main() {
 		panic(err)
 	}
 
-	// print(matrix)
-
-	fmt.Println("First Part: ", FirstPart(matrix, rows, cols))
-	// fmt.Println("Second Part: ", SecondPart(games))
+	print(matrix)
+	copiedMatrix := make([][]rune, len(matrix))
+	copy(copiedMatrix, matrix)
+	// fmt.Println("First Part: ", FirstPart(matrix, rows, cols))
+	fmt.Println("Second Part: ", SecondPart(copiedMatrix, rows, cols))
 
 	// print(matrix)
 
@@ -59,17 +60,60 @@ func FirstPart(matrix [][]rune, rows, cols int) int {
 	return total
 }
 
+func SecondPart(matrix [][]rune, rows, cols int) int {
+	total := 0
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if matrix[i][j] == '*' && hasOnlyTwoNeighbors(matrix, i, j) {
+				matrix[i][j] = '.'
+				total += lookAndMulParts(matrix, i, j)
+			}
+		}
+	}
+	return total
+}
+
+func around(row, col int) [][]int {
+	return [][]int{
+		{row - 1, col - 1}, // top left
+		{row - 1, col},     // top
+		{row - 1, col + 1}, // top right
+		{row, col + 1},     // right
+		{row + 1, col + 1}, // bottom right
+		{row + 1, col},     // bottom
+		{row + 1, col - 1}, // bottom left
+		{row, col - 1},     // left
+	}
+}
+
+func hasOnlyTwoNeighbors(matrix [][]rune, row, col int) bool {
+	neighbors := 0
+	for _, pair := range around(row, col) {
+		if unicode.IsDigit(matrix[pair[0]][pair[1]]) {
+			neighbors++
+			fmt.Println(row, col, neighbors)
+		}
+	}
+
+	return neighbors == 2
+}
+
+func lookAndMulParts(matrix [][]rune, row, col int) int {
+	partsProd := 1
+
+	for _, pair := range around(row, col) {
+		partsProd *= Check(matrix, pair[0], pair[1])
+	}
+
+	return partsProd
+}
+
 func LookAndSumParts(matrix [][]rune, row, col int) int {
 	partsSum := 0
 
-	partsSum += Check(matrix, row-1, col-1) // top left
-	partsSum += Check(matrix, row-1, col)   // top
-	partsSum += Check(matrix, row-1, col+1) // top right
-	partsSum += Check(matrix, row, col+1)   // right
-	partsSum += Check(matrix, row+1, col+1) // bottom right
-	partsSum += Check(matrix, row+1, col)   // bottom
-	partsSum += Check(matrix, row+1, col-1) // bottom left
-	partsSum += Check(matrix, row, col-1)   // left
+	for _, pair := range around(row, col) {
+		partsSum += Check(matrix, pair[0], pair[1])
+	}
 
 	return partsSum
 }
