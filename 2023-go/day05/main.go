b4e187162255fc99404bfac53f79b8ca62f35eeb
Ilea Cristian <ileacristian@gmail.com>
2024-02-04 23:49:47 +0200

    WIP Day 5 part 2 AOC Go + tests

--- 2023-go/day05/main.go
+++ 2023-go/day05/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"unicode"
@@ -75,7 +76,7 @@ func main() {
 	}
 
 	fmt.Println("First Part: ", FirstPart(seeds, mapsOrdered))
-	// fmt.Println("Second Part: ", SecondPart(cards))
+	fmt.Println("Second Part: ", SecondPart(seeds, mapsOrdered))
 }
 
 func FirstPart(seeds []int, maps []*Mapping) int {
@@ -93,6 +94,31 @@ func FirstPart(seeds []int, maps []*Mapping) int {
 	}
 
 	return minLocation
+}
+
+func SecondPart(seeds []int, maps []*Mapping) int {
+	seedRanges := []Range{}
+	for pair := 0; pair < len(seeds)/2; pair++ {
+		seedRange := Range{
+			Start:  seeds[pair*2],
+			Length: seeds[pair*2+1],
+		}
+
+		seedRanges = append(seedRanges, seedRange)
+	}
+
+	destinations := []Range(seedRanges)
+	for _, mapping := range maps {
+		newDestinations := []Range{}
+		for _, source := range destinations {
+			newDestinations = append(newDestinations, (*mapping).GetMappingRanges(source)...)
+		}
+		destinations = newDestinations
+	}
+
+	sort.Sort(ByStart(destinations))
+
+	return destinations[0].Start
 }
 
 func FillMap(line string, mapping *Mapping) {
@@ -124,11 +150,112 @@ func (m *Mapping) GetMapping(source int) int {
 	return source
 }
 
+func (m *Mapping) GetMappingRanges(sourceRange Range) []Range {
+	destinations := []Range{}
+	overlaps := []Range{}
+
+	for _, mappingInstr := range m.instructions {
+		if destination, overlap, foundOverlap := mappingInstr.mappingRange(sourceRange); foundOverlap {
+			destinations = append(destinations, destination)
+			overlaps = append(overlaps, overlap)
+		}
+	}
+
+	destinations = append(destinations, UnmappedRanges(sourceRange, overlaps)...)
+
+	return destinations
+}
+
+func UnmappedRanges(sourceRange Range, overlaps []Range) []Range {
+	ranges := []Range{}
+	sort.Sort(ByStart(overlaps))
+
+	if len(overlaps) == 0 {
+		return []Range{sourceRange}
+	}
+
+	var startPivot int
+	var currentOverlapIdx int
+	if sourceRange.Start < overlaps[0].Start {
+		startPivot = sourceRange.Start
+	} else {
+		for i, overlap := range overlaps {
+			if overlap.Start+overlap.Length-1 >= sourceRange.Start {
+				startPivot = overlap.Start + overlap.Length
+				currentOverlapIdx = i + 1
+				break
+			}
+		}
+	}
+
+	for currentOverlapIdx < len(overlaps) {
+		if overlaps[currentOverlapIdx].Start-startPivot <= 0 {
+			currentOverlapIdx++
+			continue
+		} else {
+			if overlaps[currentOverlapIdx].Start >= sourceRange.Start+sourceRange.Length {
+				ranges = append(ranges, sourceRange)
+				startPivot = sourceRange.Start + sourceRange.Length
+				break
+			} else {
+				ranges = append(ranges, Range{Start: startPivot, Length: overlaps[currentOverlapIdx].Start - startPivot})
+
+			}
+			pivotCandidate := overlaps[currentOverlapIdx].Start + overlaps[currentOverlapIdx].Length
+			if pivotCandidate > sourceRange.Start+sourceRange.Length {
+				startPivot = sourceRange.Start + sourceRange.Length
+				break
+			}
+			startPivot = pivotCandidate
+			currentOverlapIdx++
+		}
+	}
+
+	if startPivot < sourceRange.Start+sourceRange.Length {
+		ranges = append(ranges, Range{Start: startPivot, Length: sourceRange.Start + sourceRange.Length - startPivot})
+	}
+
+	return ranges
+}
+
 type MappingInstruction struct {
 	destStart   int
 	sourceStart int
 	length      int
 }
+
+type Range struct {
+	Start  int
+	Length int
+}
+
+func NewRange(start, end int) Range {
+	return Range{Start: start, Length: end - start + 1}
+}
+
+func (r *Range) End() int {
+	return r.Start + r.Length - 1
+}
+
+func (r *Range) Overlaps(other Range) bool {
+	var a, b Range
+
+	if r.Start < other.Start {
+		a = *r
+		b = other
+	} else {
+		a = other
+		b = *r
+	}
+
+	return b.Start <= a.End()
+}
+
+type ByStart []Range
+
+func (a ByStart) Len() int           { return len(a) }
+func (a ByStart) Less(i, j int) bool { return a[i].Start < a[j].Start }
+func (a ByStart) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 
 func (m MappingInstruction) mapping(source int) (destination int, found bool) {
 	if source >= m.sourceStart && source <= m.sourceStart+m.length-1 {
@@ -138,6 +265,29 @@ func (m MappingInstruction) mapping(source int) (destination int, found bool) {
 	} else {
 		destination = -1
 		found = false
+	}
+	return
+}
+
+func (m MappingInstruction) mappingRange(sourceRange Range) (destination Range, overlap Range, foundOverlap bool) {
+	end1 := sourceRange.Start + sourceRange.Length - 1
+	end2 := m.sourceStart + m.length - 1
+	overlapStart := max(sourceRange.Start, m.sourceStart)
+	overlapEnd := min(end1, end2)
+
+	if overlapStart <= overlapEnd {
+		overlapLength := overlapEnd - overlapStart + 1
+		destinationOffset := overlapStart - m.sourceStart
+		destination = Range{
+			Start:  m.destStart + destinationOffset,
+			Length: overlapLength,
+		}
+
+		overlap = Range{
+			Start:  overlapStart,
+			Length: overlapLength,
+		}
+		foundOverlap = true
 	}
 	return
 }
---
+++ 2023-go/day05/main_test.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMappingRange(t *testing.T) {
+	t.Run("Testing mappingRange with input from test file", func(t *testing.T) {
+		m := MappingInstruction{destStart: 98, sourceStart: 50, length: 2}
+		s := Range{Start: 50, Length: 2}
+		gotDest, gotOverlap, gotFound := m.mappingRange(s)
+		wantDest, wantOverlap, wantFound := Range{Start: 98, Length: 2}, Range{Start: 50, Length: 2}, true
+
+		if gotDest != wantDest || gotOverlap != wantOverlap || gotFound != wantFound {
+			t.Errorf("Expected %v %v %t but got %v %v %t", wantDest, wantOverlap, wantFound, gotDest, gotOverlap, gotFound)
+		}
+	})
+	t.Run("Testing mappingRange with no overlap", func(t *testing.T) {
+		m := MappingInstruction{destStart: 98, sourceStart: 50, length: 2}
+		s := Range{Start: 52, Length: 2}
+		gotDest, gotOverlap, gotFound := m.mappingRange(s)
+		wantDest, wantOverlap, wantFound := Range{Start: 0, Length: 0}, Range{Start: 0, Length: 0}, false
+
+		if gotDest != wantDest || gotOverlap != wantOverlap || gotFound != wantFound {
+			t.Errorf("Expected %v %v %t but got %v %v %t", wantDest, wantOverlap, wantFound, gotDest, gotOverlap, gotFound)
+		}
+	})
+
+	t.Run("Testing mappingRange with some overlap", func(t *testing.T) {
+		m := MappingInstruction{destStart: 500, sourceStart: 1, length: 100}
+		s := Range{Start: 50, Length: 100}
+		gotDest, gotOverlap, gotFound := m.mappingRange(s)
+		wantDest, wantOverlap, wantFound := Range{Start: 549, Length: 51}, Range{Start: 50, Length: 51}, true
+
+		if gotDest != wantDest || gotOverlap != wantOverlap || gotFound != wantFound {
+			t.Errorf("Expected %v %v %t but got %v %v %t", wantDest, wantOverlap, wantFound, gotDest, gotOverlap, gotFound)
+		}
+	})
+
+	t.Run("Testing mappingRange with full overlap inside", func(t *testing.T) {
+		m := MappingInstruction{destStart: 111, sourceStart: 1, length: 100}
+		s := Range{Start: 50, Length: 2}
+		gotDest, gotOverlap, gotFound := m.mappingRange(s)
+		wantDest, wantOverlap, wantFound := Range{Start: 160, Length: 2}, Range{Start: 50, Length: 2}, true
+
+		if gotDest != wantDest || gotOverlap != wantOverlap || gotFound != wantFound {
+			t.Errorf("Expected %v %v %t but got %v %v %t", wantDest, wantOverlap, wantFound, gotDest, gotOverlap, gotFound)
+		}
+	})
+}
+
+func TestGetMappingRanges(t *testing.T) {
+	t.Run("Testing Mapping with one MappingInstruction and full overlapping input Range", func(t *testing.T) {
+		m := Mapping{}
+		m.AddInstruction(MappingInstruction{destStart: 98, sourceStart: 50, length: 2})
+		s := Range{Start: 50, Length: 2}
+
+		got := m.GetMappingRanges(s)
+		want := []Range{{Start: 98, Length: 2}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Expected %v but got %v", want, got)
+		}
+	})
+
+	t.Run("Testing Mapping with one MappingInstruction and overlapping input Range", func(t *testing.T) {
+		m := Mapping{}
+		m.AddInstruction(MappingInstruction{destStart: 98, sourceStart: 50, length: 2})
+		s := Range{Start: 50, Length: 5}
+
+		got := m.GetMappingRanges(s)
+		want := []Range{{Start: 98, Length: 2}, {Start: 52, Length: 3}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Expected %v but got %v", want, got)
+		}
+	})
+
+	t.Run("Testing Mapping with multiple MappingInstruction and overlapping input Range", func(t *testing.T) {
+		m := Mapping{}
+		m.AddInstruction(MappingInstruction{destStart: 98, sourceStart: 50, length: 2})
+		m.AddInstruction(MappingInstruction{destStart: 50, sourceStart: 52, length: 48})
+
+		s := Range{Start: 79, Length: 14}
+
+		got := m.GetMappingRanges(s)
+		want := []Range{{Start: 77, Length: 14}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Expected %v but got %v", want, got)
+		}
+	})
+
+	t.Run("Testing Mapping with multiple MappingInstruction and overlapping input Range", func(t *testing.T) {
+		m := Mapping{}
+		m.AddInstruction(MappingInstruction{destStart: 15, sourceStart: 0, length: 37})
+		m.AddInstruction(MappingInstruction{destStart: 52, sourceStart: 37, length: 2})
+		m.AddInstruction(MappingInstruction{destStart: 0, sourceStart: 39, length: 15})
+
+		s := Range{Start: 77, Length: 14}
+
+		got := m.GetMappingRanges(s)
+		want := []Range{{Start: 77, Length: 14}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Expected %v but got %v", want, got)
+		}
+	})
+
+	t.Run("Testing Mapping with multiple MappingInstruction and overlapping input Range", func(t *testing.T) {
+		m := Mapping{}
+		m.AddInstruction(MappingInstruction{destStart: 15, sourceStart: 0, length: 37})
+		m.AddInstruction(MappingInstruction{destStart: 52, sourceStart: 37, length: 2})
+		m.AddInstruction(MappingInstruction{destStart: 0, sourceStart: 39, length: 39})
+
+		s := Range{Start: 77, Length: 14}
+
+		got := m.GetMappingRanges(s)
+		want := []Range{{Start: 38, Length: 1}, {Start: 78, Length: 13}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Expected %v but got %v", want, got)
+		}
+	})
+}
+
+func TestUnmappedRanges(t *testing.T) {
+	t.Run("Testing UnmappedRanges with source and one overlap at the end (already mapped range) - should return unmapped range", func(t *testing.T) {
+		got := UnmappedRanges(Range{Start: 1, Length: 100}, []Range{{Start: 51, Length: 100}})
+		want := []Range{{Start: 1, Length: 50}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Expected %v but got %v", want, got)
+		}
+	})
+
+	t.Run("Testing UnmappedRanges with source and one overlap at the beginning (already mapped range) - should return unmapped range", func(t *testing.T) {
+		got := UnmappedRanges(Range{Start: 100, Length: 100}, []Range{{Start: 51, Length: 100}})
+		want := []Range{{Start: 151, Length: 49}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Expected %v but got %v", want, got)
+		}
+	})
+
+	t.Run("Testing UnmappedRanges with source and multiple overlaps (already mapped ranges) - should return unmapped ranges", func(t *testing.T) {
+		got := UnmappedRanges(Range{Start: 1, Length: 100}, []Range{{Start: 69, Length: 3}, {Start: 42, Length: 7}})
+		want := []Range{{Start: 1, Length: 41}, {Start: 49, Length: 20}, {Start: 72, Length: 29}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Expected %v but got %v", want, got)
+		}
+	})
+
+	t.Run("Testing UnmappedRanges with source and one overlap that already covers the whole source - should return empty slice", func(t *testing.T) {
+		got := UnmappedRanges(Range{Start: 1, Length: 100}, []Range{{Start: 0, Length: 400}})
+		want := []Range{}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Expected %v but got %v", want, got)
+		}
+	})
+
+	t.Run("Testing UnmappedRanges with source and one overlap that doesn't cover anything - should return the source slice", func(t *testing.T) {
+		got := UnmappedRanges(Range{Start: 1, Length: 100}, []Range{{Start: 200, Length: 50}})
+		want := []Range{{Start: 1, Length: 100}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Expected %v but got %v", want, got)
+		}
+	})
+
+	t.Run("Testing UnmappedRanges with source and one overlap - should return the remaining range", func(t *testing.T) {
+		got := UnmappedRanges(Range{Start: 50, Length: 5}, []Range{{Start: 50, Length: 2}})
+		want := []Range{{Start: 52, Length: 3}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Expected %v but got %v", want, got)
+		}
+	})
+
+	t.Run("Testing UnmappedRanges with source and no items int he overlap array - should return the source range", func(t *testing.T) {
+		got := UnmappedRanges(Range{Start: 50, Length: 5}, []Range{})
+		want := []Range{{Start: 50, Length: 5}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Expected %v but got %v", want, got)
+		}
+	})
+}
